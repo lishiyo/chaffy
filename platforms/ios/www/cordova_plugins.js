e206196547e27a11eff705636db8506c19d3e989
ageem <mike@canoe2s-MacBook-Pro.local>
2014-08-02 16:50:24 -0400

    fixing repo

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -56,6 +56,27 @@ module.exports = [
         "clobbers": [
             "Connection"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/console-via-logger.js",
+        "id": "org.apache.cordova.console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.console/www/logger.js",
+        "id": "org.apache.cordova.console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -64,7 +85,9 @@ module.exports.metadata =
     "org.apache.cordova.geolocation": "0.3.8",
     "org.apache.cordova.device": "0.2.10",
     "org.apache.cordova.dialogs": "0.2.8",
-    "org.apache.cordova.network-information": "0.2.10"
+    "org.apache.cordova.network-information": "0.2.10",
+    "org.apache.cordova.splashscreen": "0.3.1",
+    "org.apache.cordova.console": "0.2.10-dev"
 }
 // BOTTOM OF METADATA
 });
